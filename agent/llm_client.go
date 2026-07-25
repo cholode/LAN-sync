3735ea70d15a15048cd5400f0fd02849d7285d3d
cholode <2681194748@qq.com>
2026-07-25 17:37:34 +0800

    对/llm/client.go的封装

---
+++ agent/llm_client.go
@@ -0,0 +1,10 @@
+﻿package agent
+
+import "lan-im-go/agent/llm"
+
+// 以下为向后兼容的类型别名，新代码请直接使用 agent/llm 包
+type LLMClient = llm.Client
+type ChatMessage = llm.ChatMessage
+type ChatResponse = llm.ChatResponse
+
+var NewLLMClient = llm.NewClient
